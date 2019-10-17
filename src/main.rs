c65c056b2aa9c827cb8372146e620735fcf61e4a
Pedro Castilho <cstl.px@gmail.com>
2019-10-17 20:47:48 -0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::mem;
+
+#[derive(Debug)]
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+#[derive(Debug)]
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+#[derive(Debug)]
+pub struct List {
+    head: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List {
+            head: Link::Empty,
+        }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
+    }
+}
+
+fn main() {
+    let mut list = List::new();
+    println!("{:?}", list);
+    list.push(5);
+    println!("{:?}", list);
+}
